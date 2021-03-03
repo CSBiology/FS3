2a7a3db9bb0019da5dfd7538fa89aebcd74c4f86
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-03-03 19:46:45 +0100

    Correct initialization of s3 client

--- src/FS3/S3Client.fs
+++ src/FS3/S3Client.fs
@@ -8,5 +8,6 @@ module S3Client =
         let config = new AmazonS3Config()
         config.ServiceURL <- "https://s3.uni-kl.de/"
         config.UseDualstackEndpoint <- true
+        config.ForcePathStyle <- true
         let client = new AmazonS3Client (accessKey,secretAccessKey,config)
         client
