03c894fa370eb83dea5442ebd650b6fc9ce3848e
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-04-16 13:14:14 +0200

    Add bucket management

---
+++ src/FS3/BucketManagement.fs
@@ -0,0 +1,35 @@
+﻿namespace FS3
+
+
+module BucketManagement =
+
+    open Amazon.S3
+    open Amazon.S3.Model
+    open System.IO
+
+    let private initListBuckets (client: AmazonS3Client) =
+        client.ListBucketsAsync()
+        |> Async.AwaitTask
+
+    let private initCreateBucket (client: AmazonS3Client) (bucketName: string)=
+        client.PutBucketAsync(bucketName)
+        |> Async.AwaitTask
+
+    let private initDeleteBucket (client: AmazonS3Client) (bucketName: string)=
+        client.DeleteBucketAsync(bucketName)
+        |> Async.AwaitTask
+
+    let listBuckets (client: AmazonS3Client) =
+        initListBuckets client
+        |> Async.RunSynchronously
+        |> ignore
+
+    let createBucket (client: AmazonS3Client) (bucketName: string) =
+        initCreateBucket client bucketName
+        |> Async.RunSynchronously
+        |> ignore
+
+    let deleteBucket (client: AmazonS3Client) (bucketName: string) =
+        initDeleteBucket client bucketName
+        |> Async.RunSynchronously
+        |> ignore
